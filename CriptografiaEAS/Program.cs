f134363d5c98fa0a0a8a176f4f06da77d6ec582c
esveraldo <esveraldo@hotmail.com>
2024-08-09 14:48:48 -0300

    Novo exemplo

--- CriptografiaEAS/Program.cs
+++ CriptografiaEAS/Program.cs
@@ -10,3 +10,16 @@ Console.WriteLine($"Original: {originalText}");
 Console.WriteLine($"Criptografado: {encryptedText}");
 Console.WriteLine($"Decriptografado: {decryptedText}");
 Console.WriteLine($"IV: {iv}");
+
+//CHAMADA COM IV RETIRADO DA MONTAGEM DA CRIPTOGRAFIA
+//string key = "12345678901234567890123456789012"; // 32 caracteres = 256 bits
+//var aes = new AesEncryption(key);
+
+//string originalText = "Olá, Mundo! 😀🚀"; // Texto com caracteres especiais
+
+//string encryptedText = aes.Encrypt(originalText);
+//string decryptedText = aes.Decrypt(encryptedText);
+
+//Console.WriteLine($"Original: {originalText}");
+//Console.WriteLine($"Encrypted: {encryptedText}");
+//Console.WriteLine($"Decrypted: {decryptedText}");
