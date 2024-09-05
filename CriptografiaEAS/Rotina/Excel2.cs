5a1f79a8666776e5df37601919e9e89fb527da52
esveraldo <esveraldo@hotmail.com>
2024-09-05 19:35:57 -0300

    Gravar dados mais simples

---
+++ CriptografiaEAS/Rotina/Excel2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriptografiaEAS.Rotina
+{
+    public class Excel2
+    {
+        public static void GravarDadosEmCsv(string caminhoArquivo, List<string[]> dados)
+        {
+            // Usando StreamWriter para gravar em um arquivo
+            using (StreamWriter writer = new StreamWriter(caminhoArquivo))
+            {
+                foreach (var linha in dados)
+                {
+                    // Converte cada linha de dados em um formato CSV com ; como separador
+                    string linhaCsv = string.Join(";", linha);
+
+                    // Escreve a linha no arquivo
+                    writer.WriteLine(linhaCsv);
+                }
+            }
+        }
+    }
+}
