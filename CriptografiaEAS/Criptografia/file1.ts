7f4eb0665f42a75409d49560fa782e55a502fec6
Esveraldo <esveraldo@hotmail.com>
2024-11-28 09:15:26 -0300

    Tentativa

--- CriptografiaEAS/Criptografia/file1.ts
+++ CriptografiaEAS/Criptografia/file1.ts
@@ -1,143 +1,3 @@
-//APP MODULE
-
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MsalModule, MsalService, MSAL_INSTANCE, MsalInterceptor } from '@azure/msal-angular';
-import { PublicClientApplication, InteractionType, BrowserCacheLocation } from '@azure/msal-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-
-export function MSALInstanceFactory(): PublicClientApplication {
-    return new PublicClientApplication({
-        auth: {
-            clientId: environment.azureAd.clientId,
-            authority: `${environment.azureAd.instance}${environment.azureAd.tenant}`,
-            redirectUri: environment.azureAd.redirectUri
-        },
-        cache: {
-            cacheLocation: BrowserCacheLocation.LocalStorage,
-            storeAuthStateInCookie: true
-        }
-    });
-}
-
-@NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        MsalModule.forRoot(
-            MSALInstanceFactory(),
-            {
-                interactionType: InteractionType.Redirect,
-                authRequest: {
-                    scopes: environment.azureAd.scopes
-                }
-            },
-            {
-                interactionType: InteractionType.Redirect,
-                protectedResourceMap: new Map([
-                    ['https://graph.microsoft.com/v1.0/me', environment.azureAd.scopes]
-                ])
-            }
-        )
-    ],
-    providers: [
-        {
-            provide: MSAL_INSTANCE,
-            useFactory: MSALInstanceFactory
-        },
-        MsalService,
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: MsalInterceptor,
-            multi: true
-        }
-    ],
-    bootstrap: [AppComponent]
-})
-export class AppModule { }
-
-
-//APP COMPONENT
-
-import { Component, OnInit } from '@angular/core';
-import { MsalService } from '@azure/msal-angular';
-import { InteractionStatus } from '@azure/msal-browser';
-
-@Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
-})
-export class AppComponent implements OnInit {
-    isLoggedIn = false;
-    userName: string | null = null;
-
-    constructor(private msalService: MsalService) { }
-
-    ngOnInit(): void {
-        this.msalService.instance.handleRedirectPromise().then((response) => {
-            if (response?.account) {
-                this.msalService.instance.setActiveAccount(response.account);
-                this.updateLoginStatus();
-            }
-        });
-
-        this.msalService.inProgress$.subscribe((status: InteractionStatus) => {
-            if (status === InteractionStatus.None) {
-                this.updateLoginStatus();
-            }
-        });
-    }
-
-    login(): void {
-        this.msalService.loginRedirect();
-    }
-
-    logout(): void {
-        this.msalService.logoutRedirect();
-    }
-
-    private updateLoginStatus(): void {
-        const account = this.msalService.instance.getActiveAccount();
-        this.isLoggedIn = !!account;
-        this.userName = account?.username || null;
-    }
-}
-
-
-//APP MODULE HTML
-
-<div * ngIf="isLoggedIn; else notLoggedIn" >
-    <h2>Bem - vindo, {{ userName }}</h2>
-        < button(click)="logout()" class="btn btn-danger" > Logout < /button>
-            < /div>
-
-            < ng - template #notLoggedIn >
-                <button (click)="login()" class="btn btn-primary" > Login com Microsoft < /button>
-    < /ng-template>
-
-
-
-//SERVICO
-
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
-@Injectable({
-    providedIn: 'root'
-})
-export class GraphService {
-    constructor(private http: HttpClient) { }
-
-    getUserProfile() {
-        return this.http.get('https://graph.microsoft.com/v1.0/me');
-    }
-}
 
 
 ERROR Error: Uncaught (in promise): BrowserAuthError: interaction_in_progress: Interaction is currently in progress. Please ensure that this interaction has been completed before calling an interactive API.   For more visit: aka.ms/msaljs/browser-errors
@@ -186,12 +46,147 @@ Erro ao processar redirecionamento: BrowserAuthError: uninitialized_public_clien
     at asyncToGenerator.js:19:1
     at StandardController.handleRedirectPromise(StandardController.mjs: 205: 21)
 
-ngOnInit(): void {
-    console.log('Inicializando MSAL...');
-    this.msalService.instance.initialize().then(() => {
-        console.log('MSAL inicializado com sucesso.');
 
-        // Processa redirecionamento
+
+
+
+3. Configuração no AppModule
+Certifique - se de que o AppModule está configurado corretamente para usar o MSAL Angular:
+
+typescript
+Copiar código
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { MsalModule, MsalService, MSAL_INSTANCE, MsalInterceptor } from '@azure/msal-angular';
+import { PublicClientApplication, InteractionType, BrowserCacheLocation } from '@azure/msal-browser';
+
+import { environment } from '../environments/environment';
+
+export function MSALInstanceFactory(): PublicClientApplication {
+    return new PublicClientApplication({
+        auth: {
+            clientId: environment.azureAd.clientId,
+            authority: `${environment.azureAd.instance}${environment.azureAd.tenant}`,
+            redirectUri: environment.azureAd.redirectUri
+        },
+        cache: {
+            cacheLocation: BrowserCacheLocation.LocalStorage,
+            storeAuthStateInCookie: true
+        }
+    });
+}
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        MsalModule.forRoot(
+            MSALInstanceFactory(),
+            {
+                interactionType: InteractionType.Redirect,
+                authRequest: {
+                    scopes: environment.azureAd.scopes
+                }
+            },
+            {
+                interactionType: InteractionType.Redirect,
+                protectedResourceMap: new Map([
+                    ['https://graph.microsoft.com/v1.0/me', environment.azureAd.scopes]
+                ])
+            }
+        )
+    ],
+    providers: [
+        {
+            provide: MSAL_INSTANCE,
+            useFactory: MSALInstanceFactory
+        },
+        MsalService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: MsalInterceptor,
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
+4. Componente de Login
+LoginComponent
+Crie o componente de login com a lógica para gerenciar login e logout:
+
+Arquivo login.component.ts
+typescript
+Copiar código
+import { Component } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+    isLoggedIn = false;
+    userName: string | null = null;
+
+    constructor(private msalService: MsalService) { }
+
+    ngOnInit(): void {
+        const account = this.msalService.instance.getActiveAccount();
+        this.isLoggedIn = !!account;
+        this.userName = account?.username || null;
+    }
+
+    login(): void {
+        this.msalService.loginRedirect();
+    }
+
+    logout(): void {
+        this.msalService.logoutRedirect();
+    }
+}
+HTML para LoginComponent
+Crie o arquivo login.component.html:
+
+html
+Copiar código
+    < div * ngIf="isLoggedIn; else notLoggedIn" >
+        <h2>Bem - vindo, {{ userName }}</h2>
+            < button class="btn btn-danger"(click) = "logout()" > Logout < /button>
+                < /div>
+
+                < ng - template #notLoggedIn >
+                    <button class="btn btn-primary"(click) = "login()" > Login com Microsoft < /button>
+                        < /ng-template>
+5. Processar Redirecionamento no AppComponent
+No AppComponent, processaremos o redirecionamento e garantiremos que a aplicação está pronta para login:
+
+Arquivo app.component.ts
+typescript
+Copiar código
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+
+@Component({
+    selector: 'app-root',
+    template: '<router-outlet></router-outlet>',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    constructor(private msalService: MsalService) { }
+
+    ngOnInit(): void {
+        console.log('Processando redirecionamento...');
         this.msalService.instance.handleRedirectPromise()
             .then((response) => {
                 if (response?.account) {
@@ -202,26 +197,25 @@ ngOnInit(): void {
             .catch((error) => {
                 console.error('Erro ao processar redirecionamento:', error);
             });
-    }).catch((error) => {
-        console.error('Erro ao inicializar MSAL:', error);
-    });
+    }
 }
+6. Configuração de Rotas
+Adicione as rotas para o LoginComponent no AppRoutingModule:
 
+Arquivo app - routing.module.ts
+typescript
+Copiar código
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-ngOnInit(): void {
-    console.log('Processando redirecionamento...');
-    this.msalService.instance.handleRedirectPromise()
-        .then((response) => {
-            if (response?.account) {
-                this.msalService.instance.setActiveAccount(response.account);
-                console.log('Usuário logado:', response.account.username);
-            }
-        })
-        .catch((error) => {
-            console.error('Erro ao processar redirecionamento:', error);
-        })
-        .finally(() => {
-            // Redefinir o estado de interação após o redirecionamento
-            this.interactionStatusService.setInteractionInProgress(false);
-        });
-}
+const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
